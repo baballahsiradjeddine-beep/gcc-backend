71d2a7ad17a077bc839cb2c8d1d2013dfb80a342
baballahsiradjeddine-beep <baballah.siradjeddine@gmail.com>
2026-02-24 03:26:58 +0100

    style: change dashboard font to Cairo for better Arabic support

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -49,7 +49,7 @@ class AdminPanelProvider extends PanelProvider
             ->databaseNotifications()
             ->databaseNotificationsPolling("30s")
             ->lazyLoadedDatabaseNotifications(false)
-            ->font('Outfit')
+            ->font('Cairo')
             ->globalSearchKeyBindings(['command+k', 'ctrl+k'])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
